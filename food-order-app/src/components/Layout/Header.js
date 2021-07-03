cff1d37a278d3695b18ed373b1e3f08b6550da6f
Utkarsh <utkarsh.chaturvedi@83incs>
2021-07-03 11:58:04 +0530

    required components and css done

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MealImg from '../../assets/meals.jpg'
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCartButton";
+
+const Header = props => {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>FoodPhobia</h1>
+                <HeaderCardButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={MealImg} alt="A table full of food" />
+            </div>
+        </React.Fragment>
+    )
+};
+
+export default Header; 
